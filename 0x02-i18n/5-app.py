eac7c065ad78546491b60de03b21504955726fb3
ngugimuchangi <tech.curious14@gmail.com>
2023-03-01 03:44:48 +0300

    fixed locale retrieval

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -29,7 +29,7 @@ def get_locale():
     """ Gets client's locale/region
         Checks if locale has been passed in the parameters
     """
-    locale = request.args('locale', '')
+    locale = request.args.get('locale', '')
     if locale in app.config["LANGUAGES"]:
         return locale
     return request.accept_languages.best_match(Config.LANGUAGES)
