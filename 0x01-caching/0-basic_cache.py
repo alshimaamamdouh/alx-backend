cd044a03be3e175286278e6e9d8e65da3e68121d
ngugimuchangi <tech.curious14@gmail.com>
2023-03-26 13:23:53 +0300

    reinstated annotations; added module docs

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+""" Basic caching module
+"""
 from base_caching import BaseCaching
+from typing import Any, Optional
 
 
 class BasicCache(BaseCaching):
     """ Basic caching class
     """
-    def put(self, key, item):
+    def put(self, key: Any, item: Any) -> None:
         """ Adds data to cache
             - Args:
                 - key: new entry's key
@@ -14,7 +17,7 @@ class BasicCache(BaseCaching):
         if key and item:
             self.cache_data.update({key: item})
 
-    def get(self, key):
+    def get(self, key: Any) -> Optional[Any]:
         """ Gets cache data associated with given key
             - Args:
                 - key to look for
