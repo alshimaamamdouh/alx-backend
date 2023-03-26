efaeb3c827a32b90252d2db21ce4ca40be61cb7a
ngugimuchangi <tech.curious14@gmail.com>
2023-03-26 06:52:17 +0300

    adjusted return for last index out of range

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -38,7 +38,7 @@ class Server:
         try:
             dataset = dataset[start: end]
         except IndexError:
-            dataset = []
+            dataset = dataset[start: len(dataset)]
         return dataset
 
 
